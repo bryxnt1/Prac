b90ece18ab06d2a363da35d8f201755c302773a9
Bryxnt1 <bryantperez56@gmail.com>
2023-09-03 08:40:20 -0400

    test

---
+++ Broadway minimarket/main/src/components/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import
+
+export default function navbar() {
+  return (
+    <div className='navbar'>
+      <div className='leftSide'></div>
+      <img src={} />
+      <div className='rightSide'></div>
+    </div>
+  );
+}
